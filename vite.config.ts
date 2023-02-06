a12bbe31605910cc1ed27fd2bb60a1732205f03b
Comealongwithme <comealongwithme@protonmail.com>
2023-02-06 17:13:40 +0600

    fix absolute paths

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,14 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueTypeImports from 'vite-plugin-vue-type-imports'
 
+// prod url зависит от github pages репозитория
+const baseURL = process.env.NODE_ENV === 'production' ? '/aviata/' : '/'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     VueTypeImports()
   ],
+  base: baseURL
 })
